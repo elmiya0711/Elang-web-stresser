5bef96ea10157789e2b61ae41a8f5d93eb0594fa
elmiya0711 <elmiya0711@gmail.com>
2026-05-19 00:17:36 +0700

    update

--- stress.js
+++ stress.js
@@ -14,4 +14,28 @@ const proxies = fs.readFileSync('proxy.txt', 'utf8').split('\n').filter(Boolean)
 rl.question('Enter GET URL: ', (urlGet) => {
   rl.question('Enter POST URL: ', (urlPost) => {
     rl.question('Duration (seconds): ', (durasi) => {
-      
+      rl.question('Requests per second: ', (reqPerSec) => {
+        const dataPost = { key1: 'value1', key2: 'value2' };
+        const endTime = Date.now() + parseInt(durasi) * 1000;
+
+        const interval = setInterval(() => {
+          if (Date.now() >= endTime) clearInterval(interval);
+          const randomReq = Math.random() < 0.5;
+          const proxy = proxies[Math.floor(Math.random() * proxies.length)];
+          const agent = new HttpsProxyAgent(`http:             
+          const config = { httpsAgent: agent };
+
+          if (randomReq) {
+            axios.get(urlGet, config)
+              .then(() => console.log(`GET sent via ${proxy}`))
+              .catch(e => console.error('Error:', e.message));
+          } else {
+            axios.post(urlPost, dataPost, config)
+              .then(() => console.log(`POST sent via ${proxy}`))
+              .catch(e => console.error('Error:', e.message));
+          }
+        }, 1000 / parseInt(reqPerSec));
+      });
+    });
+  });
+});
